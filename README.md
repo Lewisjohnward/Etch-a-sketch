cc8b3dccc382605b008cb2d82e061e5f5f7ce93d
Lewis Ward <lewisjohnward+git1@gmail.com>
2020-12-11 11:54:43 +0000

    update

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Etch-a-sketch
 day 0: I don't understand the DOM too well but I'm going to create a 4x4 div wall for the Etch-a-Sketch to start with and improve upon that. Scratch that, first we'll start with a single box I can't make a 4x4.
+
+day 1: added input box that asks how many what size grid from 1x1 up to 64 x 64
+
+day 2: makes new divs and trying to add a reset button that removes the divs
--- script.js
+++ script.js
@@ -1,12 +1,48 @@
-const box = document.querySelector('.box');
-box.addEventListener('click', toggleColor);
+// const box = document.querySelector('.box');
+// box.addEventListener('click', toggleColor);
 
-function toggleColor() {
-    if (document.querySelector(".box").style.backgroundColor == "black"){
-      document.querySelector(".box").style.backgroundColor = "white";
-    } else{
-        document.querySelector(".box").style.backgroundColor = "black"
-    }
+// function toggleColor() {
+//     if (document.querySelector(".box").style.backgroundColor == "black"){
+//       document.querySelector(".box").style.backgroundColor = "white";
+//     } else{
+//         document.querySelector(".box").style.backgroundColor = "black"
+//     }
+// }
+let gridSize = document.querySelector('#size-form');
+let reset = document.getElementById('gridReset');
+let sketchContainer = document.querySelector('.sketchContainer');
+gridSize.addEventListener('submit', buildGrid);
+reset.addEventListener('click',resetGrid);
+
+
+
+function resetGrid(){
+    let grid = document.querySelector('.sketchContainer');
+    grid.remove();
+        let newContainer = document.createElement('div');
+    newContainer.className = 'sketchContainer';
+    document.body.appendChild(newContainer);
+
 }
 
+function buildGrid(e){
+    e.preventDefault();
+
+    //get input value
+    let size = document.getElementById('item').value;
+    console.log(size);
+
+    for(let i = 0; i < size; i++){
+        //create X new divs. 
+        let div = document.createElement('div');
+        //add class
+        div.className = 'sketchGrid';
+        //give div a black border
+        div.style.borderColor = 'black';
+        //give dive a solid border
+        div.style.borderStyle = 'solid';
+        //append new div
+        sketchContainer.appendChild(div);
+    }
+}
 
